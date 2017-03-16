7d7a491223d01449bae0f44f89a822da6c08f1cd
ferhan <ayondelrico@gmail.com>
2017-03-16 13:03:23 +0200

    code rewritten. game works no AI

--- lib/TicTacToe.rb
+++ lib/TicTacToe.rb
@@ -1,239 +1,167 @@
-class String
-  #method to check integer within the class String
+#Possible ways to win
+WAYS_TO_WIN = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+               [0, 3, 6], [1, 4, 7], [2, 5, 8],
+               [0, 4, 8], [2, 4, 6]]
 
-  def is_int
-    self.to_i.to_s == self
+class GameBoard
+  attr_accessor :cells
+
+  def initialize
+    @cells = Array.new(9) # { nil }
+  end
+
+  def as_string
+    stringed_gameboard = @cells.map.with_index(1) do |symbol, idx|
+      symbol || idx
+    end
+
+    stringed_gameboard.each_slice(3).map do |row|
+      "  #{row.join(' | ')}"
+    end.join("\n ---+---+---\n")
+  end
+
+  def cell_open?(position)
+    @cells[position - 1].nil?
+  end
+
+  def win_game?(symbol)
+    WAYS_TO_WIN.any? do |seq|
+      seq.all? { |a| @cells[a] == symbol }
+    end
+  end
+
+  def full?
+    @cells.all?
+  end
+
+  def place_symbol(position, symbol)
+    @cells[position - 1] = symbol
   end
 end
 
-module TicTacToe
-  class Game
-
-    def initialize
-      @gameboard = [1..9].to_a
-      @running = true
-      @exit = false
-    end
-
-    def display_gameboard
-      n = 0
-      b = @gameboard
-      puts ""
-      puts " -----------"
-      3.times do
-        puts "  " + b[n].to_s + " | " + b[n+1].to_s + " | " + b[n+2].to_s
-        puts " -----------"
-        n += 3
-      end
-      puts ""
-    end
-
-    def x_turn
-      display_gameboard
-      puts "Choose number between 1 to 9 to place a mark for Player X"
-      position = gets.chomp
-      if position.is_int
-        position = position.to_i
-      end
-      if @gameboard.include?(position)
-        @gameboard.map! do |num|
-          if num == position
-            num = "X"
-          else
-            num
-          end
-        end
-
-      elsif position.is_a?(String)
-        if position.downcase == "exit"
-          puts "bye"
-          @exit = true
-          return
-        end
-        puts "ERROR. Only numbers are allowed on gameboard"
-        puts "RETRY or type EXIT to exit"
-        x_turn
-      end
-
-    end
-
-    def o_turn
-      display_gameboard
-      puts "Choose a number between 1 to 9 to place a mark for Player O"
-      position = gets.chomp
-      if position.is_int
-        position = position.to_i
-      end
-      if @gameboard.include?(position)
-        @gameboard.map! do |num|
-          if num == position
-            num = "O"
-          else
-            num
-          end
-        end
-      elsif position.is_a?(String)
-        if position.downcase == "exit"
-          puts "bye bye"
-          return @exit = true
-        end
-        puts "ERROR. Only numbers are allowed on gameboard"
-        puts "RETRY or type EXIT to exit"
-        o_turn
-      end
-    end
-
-    def win_game
-      b = @gameboard
-      # 3 to the horizontal
-      if(
-      (b[0..2].count("X") == 3 ||b[0..2].count("O") == 3) ||
-        (b[3..5].count("X") == 3 ||b[3..5].count("O") == 3)  ||
-          (b[6..8].count("X") == 3 || b[6..8].count("O") == 3)
-
-      ) then return true
-        #3 to the vertical win
-      elsif(
-      (b.values_at(0,3,6).count("X") == 3 || b.values_at(0,3,6).count("O") == 3) ||
-          (b.values_at(1,4,7).count("X") == 3 || b.values_at(1,4,7).count("O") == 3) ||
-            (b.values_at(2,5,8).count("X") == 3 || b.values_at(2,5,8).count("O") == 3)
-      )then return true
-        #two to the diagonal
-      elsif(
-      (b.values_at(0,4,8).count("X") == 3 || b.values_at(0,4,8).count("O") == 3) ||
-          (b.values_at(2,4,6).count("X") == 3 || b.values_at(2,4,6).count("O") == 3)
-
-      )then return true
-      else
-        return false
-      end
-    end
-
-    def draw_game
-      gameboard.all? { |all| all.is_a? String} #true when nobody wins
-    end
-
-    def result
-      if win_game
-        display_gameboard
-        puts "Game is OVER"
-        @running = false
-      elsif draw_game
-        display_gameboard
-        puts "Game is DRAWN"
-        @running = false
-      end
-    end
-
-    def game_status
-      until !@running
-        x_turn
-        break if @exit
-        result
-        break if !@running
-        o_turn
-        break if @exit
-        result
-      end
-    end
-
-    def try_sides
-      if @gameboard[1].is_a? Fixnum
-        return @gameboard[1] = "O"
-      elsif @gameboard[3].is_a? Fixnum
-        return @gameboard[3] = "O"
-      elsif @gameboard[5].is_a? Fixnum
-        return @gameboard[5] = "O"
-      elsif @gameboard[7].is_a? Fixnum
-        return @gameboard[7] = "O"
-      end
-    end
-
-    def try_corners
-      if @gameboard[0].is_a? Fixnum
-        return @gameboard[0] = "O"
-      elsif @gameboard[2].is_a? Fixnum
-        return @gameboard[2] = "O"
-      elsif @gameboard[6].is_a? Fixnum
-        return @gameboard[6] = "O"
-      elsif @gameboard[8].is_a? Fixnum
-        return @gameboard[8] = "O"
-      end
-    end
-
-    def npc_turn #checks the possibility to win before human wins
-      for i in range(0...9)
-        origin = @gameboard[i]
-        @gameboard[i] = "o" if @gameboard[i] != "X"
-        win_game ? return : @gameboard[i] = origin #return for early break out if win game is true
-      end
-
-      for i in range(0...9) # if not possibe to win before other player
-        origin = @gameboard[i]
-        @gameboard[i] = "X" if @gameboard[i] != "O"
-        if win_game
-          return @gameboard[i] = "O"
-        else
-          @gameboard[i] = origin
-        end
-      end
-
-      if !@gameboard[4].is_a?(String) # default place is center if winning is not possible
-        return @gameboard[4] = "O"
-      end
-
-      if @gameboard[4].is_a?(String)
-        rand > 0.499 ? try_sides || try_corners : try_corners || try_sides
-      end
-    end
-
-    def npcgame_status
-      if rand > 0.3
-        until !@running
-          x_turn
-          break if @exit
-          result
-          break if@running
-          puts "NPC is thinking."
-          sleep[0.6]
-          puts "NPC is thinking.."
-          sleep[0.6]
-          puts "NPC is thinking..."
-          sleep(0.3)
-          npc_turn
-          result
-        end
-      else
-        until !@running
-          puts "NPC is thinking."
-          sleep(0.6)
-          puts "NPC is thinking.."
-          sleep(0.6)
-          puts "NPC is thinking..."
-          sleep(0.3)
-          npc_turn
-          result
-          break if @running
-          x_turn
-          break if @exit
-          result
-        end
-      end
-    end
+# game logic
+class Game
+  def initialize
+    @gameboard = GameBoard.new
+    # default states
+    @player1 = Human.new(@gameboard, 'Player 1', 'X')
+    @player2 = Human.new(@gameboard, 'Player 2', 'O')
+    @current_player = @player1
+    greet
+    start_screen
   end
-end
-  def play
-    include TicTacToe
-    match = Game.new
-    puts "Welcome to TicTacToe by Farhan Islam. Press 1 for a two player match. Press 2 to play against CPU"
-    puts "Type EXIT to quit"
-    choice = gets.chomp.to_i
+
+  private
+
+  def start_screen(choice = gets)
+    choice.strip!.downcase! if choice
+    until %w(1 2 3 exit).include?(choice)
+      puts 'You silly goose, try again.'
+      choice = gets.chomp.downcase
+    end
+    select_game_mode(choice)
+    display_board
+    run_game
+  end
+
+  def select_game_mode(choice)
     case choice
-      when 1 then match.game_status
-      when 2 then match.npcgame_status
-      else
-        puts "Not Possible"
+      when '1'    then @player2 = Human.new(@gameboard, 'Player 2', 'O')
+      when 'exit' then exit
     end
   end
-play
 
+  def greet
+    print "\nWelcome to Tic Tac Toe.\n\n"
+    print "\nPress 1 to play against another player.\n"
+
+    print "\nType EXIT  to quit.\n"
+  end
+
+  def display_board
+    puts "\e[H\e[2J" # clear ANSI
+    print @gameboard.as_string, "\n\n"
+  end
+
+  def run_game
+    until game_over?
+      swap_players
+      check_place
+      display_board
+      if game_over?
+        puts display_result
+        exit
+      end
+    end
+  end
+
+  def game_over?
+    @gameboard.win_game?(@current_player.symbol) || @gameboard.full?
+  end
+
+  def check_place
+    position = @current_player.take_input
+    @gameboard.place_symbol(position.to_i, @current_player.symbol) unless position.nil?
+  end
+
+  def display_result
+    if @gameboard.win_game?(@current_player.symbol)
+      "Game Over Loser, #{@current_player.name} has won."
+    elsif @gameboard.full?
+      'Draw.'
+    end
+  end
+
+  def swap_players
+    case @current_player
+      when @player1 then @current_player = @player2
+      else               @current_player = @player1
+    end
+  end
+end
+
+# players in the game
+class Player
+  attr_reader :name, :symbol
+
+  def initialize(board, name, symbol)
+    @gameboard = board
+    @name = name
+    @symbol = symbol
+  end
+end
+
+# human players in the game
+class Human < Player
+  def take_input(input = nil)
+    until (1..9).include?(input) && @gameboard.cell_open?(input)
+      puts "Choose number 1 to 9 to place mark #{name}."
+      input = validate_input(gets.chomp)
+    end
+    input
+  end
+
+  private
+
+  def validate_input(input)
+    if input.to_i == 0
+      exit if input.downcase == 'exit'
+      puts 'What makes you think you can use anything but numbers? Use a number from 1 to 9'
+    else
+      position = validate_position(input.to_i)
+    end
+    position
+  end
+
+  def validate_position(position)
+    if !(1..9).include? position
+      puts 'Out of bounds.'
+      puts 'You can try again or exit'
+    elsif !@gameboard.cell_open? position
+      puts 'Place taken, you must try again'
+      puts 'Try again or exit'
+    end
+    position
+  end
+end
+Game.new
